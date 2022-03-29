b7c7cba8b40d02a1d616fb748e58aedab33a924c
Victoria Shanukova <vshanukova@gmail.com>
2022-03-29 21:36:07 +0100

    Add rendered list

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
       image: './assets/images/socks_green.jpg',
       url: 'https://github.com/shanukova',
       inventory: 0,
-      onSale: false
+      onSale: false,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ]
     }
   }
 })
