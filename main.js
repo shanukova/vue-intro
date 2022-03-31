94e7e8eee2a6beb5ce656205ece79c3a7ddb42bd
Victoria Shanukova <vshanukova@gmail.com>
2022-03-31 21:50:48 +0100

    Add computed data

--- main.js
+++ main.js
@@ -3,21 +3,39 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inStock: false,
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-      ]
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+      ],
+      onSale: true
     }
   },
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateImg(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed : {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    onSaleText() {
+      if (this.onSale) {
+        return `${this.brand} ${this.product} is on sale!`
+      }
+      return ''
     }
   }
 })
