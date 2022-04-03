e2b4de5437b912f6fe964fb92285ef15fdbd0499
Victoria Shanukova <vshanukova@gmail.com>
2022-04-03 12:07:00 +0100

    Add ReviewFrom comp

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -42,6 +42,7 @@ app.component('product-display', {
           </button>
         </div>
       </div>
+      <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
       return {
@@ -53,7 +54,8 @@ app.component('product-display', {
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
         ],
-        onSale: true
+        onSale: true,
+        reviews: []
       }
     },
     methods: {
@@ -65,6 +67,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
         this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
     },
     computed : {
